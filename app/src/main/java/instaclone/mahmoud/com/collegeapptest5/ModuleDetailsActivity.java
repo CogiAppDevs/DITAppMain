7c3d4a3d3427d396cf12919a33ea01f92ab85e76
Mahmoud Almahroum <mahmoud.al808@gmail.com>
2018-09-19 00:28:54 +0100

    Merge remote-tracking branch 'remotes/ditappremote/Timetable'
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/java/instaclone/mahmoud/com/collegeapptest5/FridayFragment.java
    #	app/src/main/java/instaclone/mahmoud/com/collegeapptest5/MondayFragment.java

--- app/src/main/java/instaclone/mahmoud/com/collegeapptest5/ModuleDetailsActivity.java
+++ app/src/main/java/instaclone/mahmoud/com/collegeapptest5/ModuleDetailsActivity.java
@@ -1,11 +1,18 @@
 package instaclone.mahmoud.com.collegeapptest5;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ModuleDetailsActivity extends AppCompatActivity {
 
@@ -14,6 +21,10 @@ public class ModuleDetailsActivity extends AppCompatActivity {
     TextView tvProfessor;
     TextView tvModule;
     ImageView backArrow;
+
+    //Shared Preference
+    SharedPreferences.Editor editor;
+    SharedPreferences cacheData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/instaclone/mahmoud/com/collegeapptest5/ModulesFragment.java
+++ app/src/main/java/instaclone/mahmoud/com/collegeapptest5/ModulesFragment.java
@@ -1,6 +1,7 @@
 package instaclone.mahmoud.com.collegeapptest5;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,14 +10,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ModulesFragment extends Fragment {
 
-    Button module1;
-    Button module2;
-    Button module3;
-    Button module4;
-    Button module5;
+    //Firebase
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference reference;
+
+    String name;
+
+    //Shared Preference
+    SharedPreferences.Editor editor;
+    SharedPreferences cacheData;
+
+    Button module1Btn;
+    Button module2Btn;
+    Button module3Btn;
+    Button module4Btn;
+    Button module5Btn;
 
     @Nullable
     @Override
@@ -24,11 +42,19 @@ public class ModulesFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_modules, container, false);
 
-        module1 = (Button)view.findViewById(R.id.module1);
-        module2 = (Button)view.findViewById(R.id.module2);
-        module3 = (Button)view.findViewById(R.id.module3);
-        module4 = (Button)view.findViewById(R.id.module4);
-        module5 = (Button)view.findViewById(R.id.module5);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        reference = firebaseDatabase.getReference();
+
+        cacheData = getActivity().getSharedPreferences(getString(R.string.sharedPrefModules), getContext().MODE_PRIVATE);
+        editor = cacheData.edit();
+
+        loadTimetable();
+
+        module1Btn = (Button)view.findViewById(R.id.module1);
+        module2Btn = (Button)view.findViewById(R.id.module2);
+        module3Btn = (Button)view.findViewById(R.id.module3);
+        module4Btn = (Button)view.findViewById(R.id.module4);
+        module5Btn = (Button)view.findViewById(R.id.module5);
 
         setupWidgets();
 
@@ -37,7 +63,7 @@ public class ModulesFragment extends Fragment {
 
     private void setupWidgets()
     {
-        module1.setOnClickListener(new View.OnClickListener() {
+       module1Btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getActivity(), ModuleDetailsActivity.class);
@@ -45,7 +71,7 @@ public class ModulesFragment extends Fragment {
             }
         });
 
-        module2.setOnClickListener(new View.OnClickListener() {
+        module2Btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getActivity(), ModuleDetailsActivity.class);
@@ -53,7 +79,7 @@ public class ModulesFragment extends Fragment {
             }
         });
 
-        module3.setOnClickListener(new View.OnClickListener() {
+        module3Btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getActivity(), ModuleDetailsActivity.class);
@@ -61,7 +87,7 @@ public class ModulesFragment extends Fragment {
             }
         });
 
-        module4.setOnClickListener(new View.OnClickListener() {
+        module4Btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getActivity(), ModuleDetailsActivity.class);
@@ -69,7 +95,7 @@ public class ModulesFragment extends Fragment {
             }
         });
 
-        module5.setOnClickListener(new View.OnClickListener() {
+        module5Btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getActivity(), ModuleDetailsActivity.class);
@@ -79,5 +105,31 @@ public class ModulesFragment extends Fragment {
 
     }
 
+    private void loadTimetable()
+    {
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference uidRef = rootRef.child(getString(R.string.modules));
 
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot ds: dataSnapshot.getChildren()) {
+
+                    if(ds.getKey().equals("module1"))
+                    {
+                        editor.putString("module1Name", dataSnapshot.child(getString(R.string.module1)).child(getString(R.string.ModuleName)).getValue(String.class));
+                        editor.apply();
+                        name = cacheData.getString("module1Name", "");
+                        module1Btn.setText(name);
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        };
+        uidRef.addListenerForSingleValueEvent(valueEventListener);
+    }
 }
